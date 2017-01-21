f7e5a7565ff3241cb92c27fbc39f5fafb523a6de
John Park <jongmanpark418@gmail.com>
2017-01-21 09:57:48 -0800

    Added whole project;

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour {
+	public void restartGame() {
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
