df399747001ecafe8a242022a1235761715f40e1
Mei <Mei@dhcp-206-87-193-109.ubcsecure.wireless.ubc.ca>
2017-01-21 19:35:43 -0800

    leo code

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -13,17 +13,16 @@ public class BulletScript : MonoBehaviour {
 		Vector3 mousePos = cam.ScreenToWorldPoint (Input.mousePosition);
 		Vector3 currentPos = transform.position;
 		speed = mousePos - currentPos;
-		if (speed.x >= 0.0f) {
-			speed.x = 3.0f;
-		} else if (speed.x < 0.0f) {
-			speed.x = -3.0f;
-		} else {
-			speed.x = 0.0f;
-		}
+//		if (speed.x > 0) {
+//			speed.x = 3;
+//		} else if (speed.x < 0) {
+//			speed.x = -3;
+//		} else {
+//			speed.x = 0;
+//		}
+		speed.z = 0;
 		rb = GetComponent<Rigidbody2D> ();
 		rb.velocity = speed;
-
-
 	}
 	
 	// Update is called once per frame
